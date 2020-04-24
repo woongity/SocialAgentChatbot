381600e5f6be37e26dbfb6a5bdc5a9445a397d58
Yang-TaeWoong <ytw5754@naver.com>
2020-04-24 09:13:04 +0000

    data

--- application.py
+++ application.py
@@ -9,17 +9,19 @@ app = Flask(__name__)
 
 
 def get_url(location):
-    data = {"검암경서동" : "gumam" , "연희동" : "yeonhi", "청라1동" : "cheongna1", "청라3동" : "cheongna3", "청라2동" :"cheongna2","가정1동":"gajeong1", "가정2동" :",gajeong2" , "가정3동" : "gajeong3", "신현원창동" : "sinhyun", "석남1동" :"seoknam1", "석남2동" : "seoknam2", "석남3동" : "seoknam3" , "가좌1동" :"gajwa1", "가좌2동" :"gajwa2" , "가좌3동" : "gajwa3" , "가좌4동" :"gajwa4", "검단동" :"gumdan1", "불로대곡동" :"gumdan2","원당동":"gumdan3", "당하동":"gumdan4","마전동":"majeon", "오류왕길동":"gumdan5"}
+    data = {"검암경서동": "gumam", "연희동": "yeonhi", "청라1동": "cheongna1", "청라3동": "cheongna3", "청라2동": "cheongna2", "가정1동": "gajeong1", "가정2동" :",gajeong2" , "가정3동" : "gajeong3", "신현원창동" : "sinhyun", "석남1동" :"seoknam1", "석남2동" : "seoknam2", "석남3동" : "seoknam3" , "가좌1동" :"gajwa1", "가좌2동" :"gajwa2" , "가좌3동" : "gajwa3" , "가좌4동" :"gajwa4", "검단동" :"gumdan1", "불로대곡동" :"gumdan2","원당동":"gumdan3", "당하동":"gumdan4","마전동":"majeon", "오류왕길동":"gumdan5"}
     if location in data:
         return data[location]
-    return None
-
-
+    else:
+        return False
+# 딱맞는 장소를 입력하지 않는다면, most_similar.py에 있는 most_similar_list 함수를 통해 가장 비슷한 것들 배열을 호출.
+# 딱맞는 장소 입력시 리턴값 : str, 비슷한 장소 입력시 리턴값 : list
 @app.route('/paper', methods=['POST'])
 def paper():
     req = request.get_json()
     paper_cata = req["action"]["detailParams"]["paper"]["value"]
-    order_command = req["action"]["detailParams"]["command"]["value"]
+    paper_cata.replace(" ", "")
+    # order_command = req["action"]["detailParams"]["command"]["value"]
     if papers.get_db_row(paper_cata) is not False:
         print(papers.get_db_row(paper_cata), sys.stderr)
     res = {
@@ -27,7 +29,7 @@ def paper():
         "template": {
         "outputs": [
                 {
-                    "simpleText":{
+                    "simpleText" : {
                         "text" : papers.get_db_row(paper_cata)
                     }
                 }
@@ -36,11 +38,14 @@ def paper():
     }
     return jsonify(res)
 
+# 유사도를 비교해서 유사 내용이 많다면 나온다면 재질문으로 버튼을 등록.
 @app.route('/noti', methods=['POST'])
 def noti():
     req = request.get_json()
     location = req["action"]["detailParams"]["sys_location"]["value"]
-    if get_url(location) is None:
+    command = req["action"]["detailParams"]["noti"]["value"]
+    # TODO : command가 알려줘라면 noti로, 신청이라면 paper로
+    if get_url(location) is False:
         url = "cheongna2"
     else:
         url = get_url(location)
--- most_similar.py
+++ most_similar.py
@@ -1,3 +1,6 @@
+
+data = {"검암경서동": "gumam", "연희동": "yeonhi", "청라1동": "cheongna1", "청라3동": "cheongna3", "청라2동": "cheongna2", "가정1동": "gajeong1", "가정2동" :",gajeong2" , "가정3동" : "gajeong3", "신현원창동" : "sinhyun", "석남1동" :"seoknam1", "석남2동" : "seoknam2", "석남3동" : "seoknam3" , "가좌1동" :"gajwa1", "가좌2동" :"gajwa2" , "가좌3동" : "gajwa3" , "가좌4동" :"gajwa4", "검단동" :"gumdan1", "불로대곡동" :"gumdan2","원당동":"gumdan3", "당하동":"gumdan4","마전동":"majeon", "오류왕길동":"gumdan5"}
+
 def get_bigram(string):
     s = string.replace(' ', '')
     return [s[i:i+2] for i in range(len(string)-1)]
@@ -16,14 +19,27 @@ def get_similarity(str1, str2):
     return (2.0 * hit_count) / union
 
 
-def max_similarity_chr(string, lists):
+def pop_all(lists):
+    while(len(lists) > 0):
+        lists.pop()
+
+
+def most_similar_list(string):
     maximum = 0
-    maximum_index = 0
-    for i in lists:
-        if maximum < get_similarity(string, i):
-            maximum = get_similarity(string, i)
-            maximum_index = i
-    if maximum == 0:
-        return False
-    else:
-        return maximum_index
+    previous = 0
+    stack = []
+    for key, value in data:
+        if maximum < get_similarity(key, string):
+            maximum = get_similarity(key, string)
+        if previous < maximum:
+            pop_all(stack)
+            stack.append({string: value})
+        if previous == maximum:
+            stack.append({string: value})
+        previous = get_similarity(string, stack[-1][key])
+        ''' 이전에 위치한 similarity 값을 previous에,
+        현재 위치한 값의 similarity를 stack에 넣으면서 previous > current_simil 이라면 pass,
+        previous==current_simil라면 append,
+        previous < current_simil이라면 pop all and append
+        '''
+    return stack
